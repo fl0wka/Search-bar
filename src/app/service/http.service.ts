f89b0f58efe0b859517c4d06408af730bdfb0daf
fl0wka <vadimus-garbage@mail.ru>
2023-09-12 23:27:20 +0200

    some fixed

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -3,9 +3,9 @@ import configFile from '../config.json';
 
 export const apiGithub = axios.create({
   baseURL: configFile.apiEndpoitGithub,
-  // headers: {
-  //   Authorization: process.env.REACT_APP_SECRET_KEY_GITHUB,
-  // },
+  headers: {
+    Authorization: process.env.REACT_APP_SECRET_KEY_GITHUB,
+  },
 });
 export const apiYoutube = axios.create({
   baseURL: configFile.apiEndpoitYoutube,
